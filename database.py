00f951dbf41dafe7d8996e41765fff63e5c6f1fc
kishore6002 <glokeshgkishore4@gmail.com>
2026-01-15 21:55:20 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,27 +1,29 @@
 import sqlite3
 
-db = sqlite3.connect("database.db")
+def init_db():
+    db = sqlite3.connect("database.db")
+    
+    db.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        username TEXT,
+        password TEXT,
+        dept TEXT,
+        role TEXT
+    )
+    """)
 
-db.execute("""
-CREATE TABLE IF NOT EXISTS users (
-    username TEXT,
-    password TEXT,
-    dept TEXT,
-    role TEXT
-)
-""")
+    db.execute("""
+    CREATE TABLE IF NOT EXISTS allotments (
+        dept TEXT,
+        year TEXT,
+        section TEXT,
+        room TEXT,
+        day TEXT
+    )
+    """)
 
-db.execute("""
-CREATE TABLE IF NOT EXISTS allotments (
-    dept TEXT,
-    year TEXT,
-    section TEXT,
-    room TEXT,
-    day TEXT
-)
-""")
+    db.commit()
+    db.close()
 
-db.commit()
-db.close()
+    print("Database ready")
 
-print("Database ready")
